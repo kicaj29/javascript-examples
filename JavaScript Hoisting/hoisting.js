a00f221a3333b39e7851cca3e7e415e796aa746e
Kowalski, Jacek <jacek.kowalski@dieboldnixdorf.com>
2017-09-16 13:07:20 +0200

    hoisting example

--- JavaScript Hoisting/hoisting.js
+++ JavaScript Hoisting/hoisting.js
@@ -1,7 +1,9 @@
+'use strict';
+
 function JavaScriptDeclarationsAreHoisted_Example1() {
     x = 5; // Assign 5 to x
 
-    elem = document.getElementById("demo"); // Find an element
+    var elem = document.getElementById("demo"); // Find an element
     elem.innerHTML = x;                     // Display x in the element
 
     var x; // Declare x
@@ -11,7 +13,7 @@ function JavaScriptDeclarationsAreHoisted_Example2() {
     var x; // Declare x
     x = 5; // Assign 5 to x
 
-    elem = document.getElementById("demo"); // Find an element
+    var elem = document.getElementById("demo"); // Find an element
     elem.innerHTML = x;                     // Display x in the element
 }
 
@@ -19,16 +21,120 @@ function  JavaScriptInitializationsAreNotHoisted_Example1() {
     var x = 5; // Initialize x
     var y = 7; // Initialize y
 
-    elem = document.getElementById("demo"); // Find an element
+    var elem = document.getElementById("demo"); // Find an element
     elem.innerHTML = x + " " + y;           // Display x and y
 }
 
 function  JavaScriptInitializationsAreNotHoisted_Example2() {
     var x = 5; // Initialize x
-    var y;
 
-    elem = document.getElementById("demo"); // Find an element
+    //yy = 15; //this will cause error in run time if we use strict mode!!!
+    y = 5;
+    var elem = document.getElementById("demo"); // Find an element
     elem.innerHTML = x + " " + y;           // Display x and y
 
-    y = 7; // Initialize y
+    var y = 7; // Initialize y
+}
+
+function hoist() {
+    a = 20;
+    var b = 100;
+}
+hoist();
+
+function AllUndeclaredVariablesAreGlobalVariables() {
+
+    /**
+     * Accessible as a global variable outside hoist() function
+     * Output: 20
+     */
+    console.log(a);
+    /**
+     * Since it was declared, it is confined to the hoist() function scope.
+     * We can't print it out outside the confines of the hoist() function.
+     * Output: ReferenceError: b is not defined
+     */
+    console.log(b);
+}
+
+function LetVsVar() {
+    console.log(myhoist);
+    let myhoist = 'The variable has been hoisted.';   //let is not hoisted so we get an error: Uncaught ReferenceError: hoist is not defined
+    //var myhoist = 'The variable has been hoisted.'; //it works always, variable hoist is hoisted so we get undefined
+}
+
+function ClassDeclarationsExample1() {
+    var Frodo = new Hobbit();
+    Frodo.height = 100;
+    Frodo.weight = 300;
+    console.log(Frodo); // Output: ReferenceError: Hobbit is not defined
+
+    class Hobbit {
+        constructor(height, weight) {
+            this.height = height;
+            this.weight = weight;
+        }
+    }
+}
+
+function ClassDeclarationsExample2() {
+    class Hobbit {
+        constructor(height, weight) {
+            this.height = height;
+            this.weight = weight;
+        }
+    }
+
+    var Frodo = new Hobbit();
+    Frodo.height = 100;
+    Frodo.weight = 300;
+    console.log(Frodo); // Output: ReferenceError: Hobbit is not defined
+}
+
+
+
+function classVsFunction() {
+    debugger;
+    var fooF = new BarF();
+    console.log(fooF.x)     // here we get undefined!
+    BarF.defaultX = 0;
+
+
+    var fooC = new BarC(); // ReferenceError, move class BarC up to not have this error
+    console.log(fooC.x);
+    BarC.defaultX = 0;
+
+    function BarF(x) {
+        this.x = x || BarF.defaultX;
+    }
+
+    class BarC {
+        constructor (x = BarC.defaultX) {
+            this.x = x;
+        }
+    }
+}
+
+function sampleWhyClassesAreNotHoisted() {
+
+    /**
+     * If class Dog were hoisted to the top the code would break at the moment where the extends Animal is interpreted
+     * because Animal is undeclared at that moment. The important thing to note here is that the extends part has to be
+     * evaluated at the right point in time. Therefore classes aren’t hoisted.
+     */
+    debugger;
+    let Animal = function Animal() {
+        this.move = function () {
+            alert(defaultMove);
+        }
+    }
+
+    class Dog extends Animal{
+
+    }
+
+    let defaultMove = "moving";
+
+    let dog = new Dog();
+    dog.move();
 }
--- README.md
+++ README.md
@@ -7,5 +7,24 @@ In JavaScript, a variable can be declared after it has been used. In other words
 To understand this, you have to understand the term "hoisting".   
 __Hoisting is JavaScript's default behavior of moving all declarations to the top of the current scope (to the top of the current script or the current function).__
 
+### JavaScript Initializations are Not Hoisted
+JavaScript only hoists declarations, not initializations.
+
+### Strict Mode
+Thanks to a utility of the es5 version of JavaScript known as strict-mode, we can be more careful about how we declare our variables.
+By enabling strict mode, we opt into a restricted variant of JavaScript that will not tolerate the usage of variables if they are not explicate declared.      
+NOTE: hoisting is still working but if declaration is missing at all we will ger an error in run time. It means that
+implicate declarations are disabled!
+
+#### ECMAScript 6 - let
+Variables declared with the keyword let are block scoped and not function scoped.   
+That's significant, but it shouldn't trouble us.   Briefly, however, it just means that the variable's scope is bound to the block in which it is declared and not the function in which it is declared.   
+      
+### Hoisting classes
+Classes are hoisted but they are not initialised! It means that we cannot create instance of a class if class initialization was not executed!
+NOTE: some people say that because of this we should say that they are not hoisted :)
+
 https://www.w3schools.com/js/js_hoisting.asp   
-https://stackoverflow.com/questions/35537619/why-are-es6-classes-not-hoisted   
+https://stackoverflow.com/questions/35537619/why-are-es6-classes-not-hoisted    
+https://blog.thoughtram.io/angular/2015/09/03/forward-references-in-angular-2.html     
+https://scotch.io/tutorials/understanding-hoisting-in-javascript   
